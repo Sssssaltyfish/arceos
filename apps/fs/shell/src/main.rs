87b7cb1157b4a53a063f2e88fba6d92046b9cdbd
Sssssaltyfish <47731209+Sssssaltyfish@users.noreply.github.com>
2023-10-22 02:28:42 +0800

    Reimplemented apps/fs/shell using noline

--- apps/fs/shell/src/main.rs
+++ apps/fs/shell/src/main.rs
@@ -23,22 +23,70 @@ mod cmd;
 #[cfg(feature = "use-ramfs")]
 mod ramfs;
 
-use std::io::prelude::*;
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, prelude::*, BufReader, Stdin, Stdout},
+};
 
-const LF: u8 = b'\n';
-const CR: u8 = b'\r';
-const DL: u8 = b'\x7f';
-const BS: u8 = b'\x08';
-const SPACE: u8 = b' ';
+use noline::builder::EditorBuilder;
 
 const MAX_CMD_LEN: usize = 256;
 
-fn print_prompt() {
-    print!(
-        "arceos:{}$ ",
-        path_to_str!(std::env::current_dir().unwrap())
-    );
-    std::io::stdout().flush().unwrap();
+struct Stdio<'a>(&'a mut Stdin, &'a mut Stdout);
+
+impl noline::sync::Read for Stdio<'_> {
+    type Error = std::io::Error;
+
+    fn read(&mut self) -> Result<u8, Self::Error> {
+        let mut buf = [0u8];
+        self.0.read_exact(&mut buf)?;
+        Ok(buf[0])
+    }
+}
+
+impl noline::sync::Write for Stdio<'_> {
+    type Error = std::io::Error;
+
+    fn write(&mut self, buf: &[u8]) -> Result<(), Self::Error> {
+        self.1.write_all(buf)?;
+        Ok(())
+    }
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        self.1.flush()?;
+        Ok(())
+    }
+}
+
+fn prompt_history() -> std::io::Result<File> {
+    const HISTORY_PATH: &str = "/shell.history";
+    macro_rules! is_not_found {
+        ( $e:expr ) => {{
+            #[cfg(feature = "axstd")]
+            {
+                matches!($e, io::Error::NotFound)
+            }
+            #[cfg(not(feature = "axstd"))]
+            {
+                matches!($e.kind(), io::ErrorKind::NotFound)
+            }
+        }};
+    }
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(HISTORY_PATH)
+        .or_else(|e| {
+            if is_not_found!(e) {
+                OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create_new(true)
+                    .open(HISTORY_PATH)
+            } else {
+                Err(e)
+            }
+        })
 }
 
 #[cfg_attr(feature = "axstd", no_mangle)]
@@ -46,40 +94,37 @@ fn main() {
     let mut stdin = std::io::stdin();
     let mut stdout = std::io::stdout();
 
-    let mut buf = [0; MAX_CMD_LEN];
-    let mut cursor = 0;
+    let mut io = Stdio(&mut stdin, &mut stdout);
+
+    let mut editor = EditorBuilder::new_static::<MAX_CMD_LEN>()
+        .with_static_history::<25>()
+        .build_sync(&mut io)
+        .unwrap();
+
+    let history = BufReader::new(prompt_history().unwrap())
+        .lines()
+        .filter_map(|s| s.ok());
+    editor.load_history(history);
+
     cmd::run_cmd("help".as_bytes());
-    print_prompt();
 
     loop {
-        if stdin.read(&mut buf[cursor..cursor + 1]).ok() != Some(1) {
-            continue;
+        let prompt = format!(
+            "arceos:{}$ ",
+            path_to_str!(std::env::current_dir().unwrap())
+        );
+        if let Ok(line) = editor.readline(&prompt, &mut io) {
+            cmd::run_cmd(line.as_bytes());
+        } else {
+            break;
         }
-        if buf[cursor] == b'\x1b' {
-            buf[cursor] = b'^';
+    }
+
+    let mut history = prompt_history().unwrap();
+    for slice in editor.get_history() {
+        for (_, &c) in slice {
+            history.write_all(&[c]).unwrap();
         }
-        match buf[cursor] {
-            CR | LF => {
-                println!();
-                if cursor > 0 {
-                    cmd::run_cmd(&buf[..cursor]);
-                    cursor = 0;
-                }
-                print_prompt();
-            }
-            BS | DL => {
-                if cursor > 0 {
-                    stdout.write_all(&[BS, SPACE, BS]).unwrap();
-                    cursor -= 1;
-                }
-            }
-            0..=31 => {}
-            c => {
-                if cursor < MAX_CMD_LEN - 1 {
-                    stdout.write_all(&[c]).unwrap();
-                    cursor += 1;
-                }
-            }
-        }
+        history.write_all(&[b'\n']).unwrap();
     }
 }
